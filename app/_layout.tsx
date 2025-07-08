a44a49e3e76403b6778f01f34c29ac9951eefeed
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-07-08 13:06:17 +0530

    Fix RazorpayPayment import error by removing premium references

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,7 +20,6 @@ function RootLayoutContent() {
         <Stack.Screen name="screener/results" />
         <Stack.Screen name="chat" />
         <Stack.Screen name="premium/success" />
-        <Stack.Screen name="edit-profile" />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style={isDark ? "light" : "dark"} />
